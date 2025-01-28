2993f2c5e87ded579d9d799c509ae8ac144ed978
FakeGTs <165804693+FakeGTs@users.noreply.github.com>
2025-01-28 11:56:47 +0100

    VerificaRecuperoAntonini

--- es1.py
+++ es1.py
@@ -1,30 +1,100 @@
-vendite_reparto=(
-    ("Informatica", [("gennaio", 1000),("febbraio", 235),("marzo", 12),("aprile", 453),("maggio", 864),("giugno", 123) ]),
-    ("Cibo", [("gennaio", 1000),("febbraio", 235),("marzo", 10453),("aprile", 5),("maggio", 1250),("giugno", 2425) ]),
-    ("Elettronica", [("gennaio", 1231),("febbraio", 34),("marzo", 1246),("aprile", 39),("maggio", 36),("giugno", 89) ]),
-    ("Frutta", [("gennaio", 458),("febbraio", 1234),("marzo", 4321),("aprile", 456),("maggio", 987),("giugno", 4346) ]) 
-    )
+tupla_partite = (
+    ("GiocatoreA", "GiocatoreB", 3, 2),
+    ("GiocatoreC", "GiocatoreD", 2, 3),
+    ("GiocatoreB", "GiocatoreC", 3, 0),
+    ("GiocatoreA", "GiocatoreD", 3, 1),
+    ("GiocatoreB", "GiocatoreD", 2, 3),
+)
+def media_set_partita(tupla_partite):
+  somma=0
+  conta=0
+  for dati in tupla_partite:
+    giocatore1,giocatore2,setVinti1,setVinti2=dati
+    somma+=(setVinti1+setVinti2)
+    conta+=2
+  if(conta>0):
+    media=somma/conta
+    print(round(media,2))
+  else:
+    print("errore nel calcolo della media")    
 
-def media_vendite(reparto):
-    max=0
-    min=100000
-    somma=0
-    c=0
-    for reparto,dati in vendite_reparto:
-         if nuovo_reparto==reparto: 
-            for mese,valore in dati:
-                if valore>max:
-                    max=valore
-                    meseMax=mese
-                elif valore<min :
-                    min=valore
-                    meseMin=mese
-                if valore!="N/D" :
-                    somma+=valore
-                    c+=1
-    media_totale=somma/c                          
-    print(media_totale,(max,meseMax),(min,meseMin))
+def media_set_giocatore(tupla_partite, giocatore):
+  somma=0
+  conta=0
+  for dati in tupla_partite:
+    giocatore1,giocatore2,setVinti1,setVinti2=dati
+    if(giocatore==giocatore1):
+        somma+=setVinti1
+        conta+=2
+    elif(giocatore==giocatore2):
+        somma+=setVinti2
+        conta+=2
+  if(conta>0):
+    media=somma/conta
+    print(round(media,2))
+  else:
+    print("errore nel calcolo della media") 
 
+def match_piu_combattuto(tupla_partite):
+  max_combattuto=0
+  match_combattuto=()
+  for dati in tupla_partite:
+    giocatore1,giocatore2,setVinti1,setVinti2=dati
+    if(setVinti1+setVinti2>max_combattuto):
+      max_combattuto=setVinti1+setVinti2
+      match_combattuto=(giocatore1,giocatore2,setVinti1,setVinti2)
+      print(match_combattuto)
+      if(max_combattuto==0):
+        print("errore nel calcolo del match piu combattuto")
 
-nuovo_reparto=input("inserisci un reparto da analizzare: ")
-media_vendite(nuovo_reparto)
+def match_meno_combattuto(tupla_partite): 
+  min_combattuto=10
+  match_combattuto=()
+  for dati in tupla_partite:
+    giocatore1,giocatore2,setVinti1,setVinti2=dati
+    if(setVinti1+setVinti2<min_combattuto):
+      min_combattuto=setVinti1+setVinti2
+      match_combattuto=(giocatore1,giocatore2,setVinti1,setVinti2)
+      print(match_combattuto)
+      if(min_combattuto==0):
+        print("errore nel calcolo del match piu combattuto") 
+
+def percentuale_vittorie_giocatore(tupla_partite, giocatore):
+  somma=0
+  sommaTotale=0
+  for dati in tupla_partite:
+    giocatore1,giocatore2,setVinti1,setVinti2=dati
+    if(giocatore==giocatore1):
+        sommaTotale+=setVinti1+setVinti2
+        somma+=setVinti1
+        
+    elif(giocatore==giocatore2):
+        sommaTotale+=setVinti1+setVinti2
+        somma+=setVinti2    
+  percentuale=(somma/sommaTotale)*100
+  print(giocatore,"ha una percentuale di vincita dell: ",percentuale)
+
+  
+while(True):
+  print("1: media_set_partita")
+  print("2: media_set_partita")
+  print("3: media_set_partita ")
+  print("4: media_set_partita ")
+  print("5: media_set_partita")
+  print("inserisci 0 per terminare")
+  scelta=input("Fai una scelta da 0 a 5")
+  if(scelta==1):
+    media_set_partita(tupla_partite)
+  elif(scelta==2):
+    giocatore=input("inserisci il giocatore scelto: ")
+    media_set_giocatore(tupla_partite, giocatore)
+  elif(scelta==3):
+    match_piu_combattuto(tupla_partite)
+  elif(scelta==4):
+    match_meno_combattuto(tupla_partite)
+  elif(scelta==5):
+    giocatore=input("inserisci il giocatore scelto: ")
+    percentuale_vittorie_giocatore(tupla_partite, giocatore)
+  elif(scelta==0):
+    break  
+  
--- es2.py
+++ es2.py
@@ -1,71 +1,51 @@
-tupla_pluviometrica = (
-                      (("Vittuone","Milano"),(2022, ("gennaio",20))),
-                      (("Vittuone","Milano"),(2023, ("marzo",80))),
-                      (("Vittuone","Milano"),(2023, ("aprile",60))),
-                      (("Vittuone","Milano"),(2023, ("maggio",80))),
-                      (("Vittuone","Milano"),(2023, ("luglio",30))),
-                      (("Vittuone","Milano"),(2023, ("agosto","N/D"))),
-                      (("Varenna","Lecco"),(2023, ("luglio",150))),
-                      (("Morbegno","Sondrio"),(2023, ("luglio",165)))
-                      )
+#Dati dei corsi organizzati in diverse citta
+corsi = (
+    ("Milano", [
+        ("gennaio", ("online", 50)),
+        ("gennaio", ("in presenza", 30)),
+        ("febbraio", ("online", 40)),
+        ("febbraio", ("in presenza", 25)),
+        ("marzo", ("online", 30)),
+        ("marzo", ("in presenza", 30)),
+    ]),
+    ("Bologna", [
+        ("gennaio", ("online", 45)),
+        ("gennaio", ("in presenza", 20)),
+        ("febbraio", ("online", 35)),
+        ("febbraio", ("in presenza", 30)),
+        ("marzo", ("online", 80)),
+        ("marzo", ("in presenza", 10)),    
+    ]),
+    ("Roma", [
+        ("gennaio", ("online", 45)),
+        ("gennaio", ("in presenza", 20)),
+        ("febbraio", ("online", 35)),
+        ("febbraio", ("in presenza", 30)),
+        ("marzo", ("online", 60)),
+        ("marzo", ("in presenza", 70)),    
+    ]),
+)
 
+def analizza_partecipanti(cittaScelta,modalitaScelta):
+    for citta, dati in corsi:
+        if citta==cittaScelta:
+            partecipanti_totali=0
+            conta=0
+            max_partecipanti=0
+            mese_max_partecipanti=""
+            for mese, (modalita, partecipanti) in dati:
+                if modalitaScelta==modalita:
+                    partecipanti_totali+=partecipanti
+                    conta+=1
+                    if partecipanti>max_partecipanti:
+                        max_partecipanti=partecipanti
+                        mese_max_partecipanti=mese        
+            if conta>0:
+                media_partecipanti=partecipanti_totali/conta  
+                print (round(media_partecipanti,1),(max_partecipanti, mese_max_partecipanti))
+            else:
+                return (0, (0, "nessun mese"))
 
-def media_globale(tupla_pluviometrica):
-    c=0
-    for posizione,anno,dati in tupla_pluviometrica:
-        for (mese,valore) in dati:
-            if valore!="N/D" :
-                somma=sum(valore)
-                c+=1
-    media_totale=somma/c
-    print("La media totale è: ",media_totale)
-
-def media(tupla_pluviometrica,provincia,mese):
-    c=0
-    for (paese,provincia),anno,dati in tupla_pluviometrica:
-        if nuovo_paese==paese and nuova_provincia==nuova_provincia:
-            for (mese,valore) in dati:
-                if valore!="N/D" :
-                    somma=sum(valore)
-                    c+=1
-    media_posizione=somma/c
-    print("La media totale è: ",media_posizione)    
-
-def pioggiaMax(provincia):
-        ValMax=0
-        for (paese,provincia),anno,dati in tupla_pluviometrica:
-            for (mese,valore) in dati:
-                ValMax=max(valore)
-                meseMax=mese
-            print((meseMax,ValMax))
-
-
-def pioggiaMin(provincia):
-        ValMax=0
-        for (paese,provincia),anno,dati in tupla_pluviometrica:
-            for (mese,valore) in dati:
-                ValMin=min(valore)
-                meseMin=mese
-            print((meseMin,ValMin))
-
-
-
-scelta=input(int("1. Media Globale | 2. Media per provincia e paese | 3.Massimo di pioggia per provincia | 4.Minimo di pioggia per provincia"))
-
-while scelta>0 and scelta <5:
-    if(scelta==1):
-        media_globale(tupla_pluviometrica) 
-        
-    elif(scelta==2):
-        nuova_provincia=input("inserisci una provincia da analizzare: ")
-        nuovo_paese=input("inserisci un paese da analizzare: ")
-        media(tupla_pluviometrica,nuova_provincia,nuovo_paese)
-            
-    elif (scelta==3):
-        pioggiaMax("Milano")
-        
-    elif (scelta==4) :
-        pioggiaMin("Milano")
-
-    else:
-        print("Scelta errata")
+cittaScelta=input("Scegli una citta: ")
+modalita=input("Scegli una modalita: ")
+analizza_partecipanti(cittaScelta,modalita)
